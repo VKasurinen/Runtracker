1cf1964605bada9be9dc8ad55b28fe5aa6b534e1
VKasurinen <vaino.kasurinen@hotmail.fi>
2024-05-06 15:39:38 +0300

    gradle conventionplugins and configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,47 +1,17 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.runique.android.application.compose)
+    alias(libs.plugins.runique.jvm.ktor)
     alias(libs.plugins.mapsplatform.secrets.plugin)
 }
 
 android {
     namespace = "com.example.runique"
-    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.runique"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
     }
     packaging {
         resources {
@@ -105,6 +75,5 @@ dependencies {
     implementation(projects.run.data)
     implementation(projects.run.location)
     implementation(projects.run.network)
-
 
 }
---
+++ build-logic/convention/src/main/java/JvmKtorConventionPlugin.kt
@@ -0,0 +1,24 @@
+
+import androidx.room.gradle.RoomExtension
+import com.android.build.gradle.LibraryExtension
+import com.example.convention.configureAndroidCompose
+import com.example.convention.configureKotlinJvm
+import com.example.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+class JvmKtorConventionPlugin: Plugin<Project> {
+
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.apply("org.jetbrains.kotlin.plugin.serialization")
+
+            dependencies {
+                "implementation"(libs.findBundle("ktor").get())
+            }
+        }
+    }
+}

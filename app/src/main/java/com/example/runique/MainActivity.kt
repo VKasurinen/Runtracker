da5abb9a2ced7b79f80a131763797b9e50c4c215
VKasurinen <vaino.kasurinen@hotmail.fi>
2024-05-07 19:22:31 +0300

    Project utilities

--- app/src/main/java/com/example/runique/MainActivity.kt
+++ app/src/main/java/com/example/runique/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.runique.ui.theme.RuniqueTheme
+import com.example.core.presentation.designsystem.AnalyticsIcon
+import com.example.core.presentation.designsystem.RuniqueTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,25 +25,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Icon(imageVector = AnalyticsIcon, contentDescription = null)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RuniqueTheme {
-        Greeting("Android")
-    }
-}
--- core/domain/src/main/java/com/example/core/domain/util/Error.kt
+++ core/domain/src/main/java/com/example/core/domain/util/Error.kt
@@ -1,2 +1,3 @@
 package com.example.core.domain.util
 
+interface Error

9410d2aa51eb9767b7e4efc857cd4168bcf57030
VKasurinen <vaino.kasurinen@hotmail.fi>
2024-12-11 17:56:13 +0200

    ambient mode to watch

--- Wear/run/presentation/src/main/java/com/example/wear/run/presentation/TrackerScreen.kt
+++ Wear/run/presentation/src/main/java/com/example/wear/run/presentation/TrackerScreen.kt
@@ -47,6 +47,8 @@ import com.example.core.presentation.ui.ObserveAsEvents
 import com.example.core.presentation.ui.formatted
 import com.example.core.presentation.ui.toFormattedHeartRate
 import com.example.core.presentation.ui.toFormattedKm
+import com.example.wear.run.presentation.ambient.AmbientObserver
+import com.example.wear.run.presentation.ambient.ambientMode
 import com.example.wear.run.presentation.components.RunDataCard
 import org.koin.androidx.compose.koinViewModel
 
@@ -58,16 +60,13 @@ fun TrackerScreenRoot(
     val context = LocalContext.current
     val state = viewModel.state
     val isServiceActive by ActiveRunService.isServiceActive.collectAsStateWithLifecycle()
-    LaunchedEffect(state.isRunActive, state.hasStartedRunning, ActiveRunService.isServiceActive) {
-
-        if(state.isRunActive && !isServiceActive) {
+    LaunchedEffect(state.isRunActive, state.hasStartedRunning, isServiceActive) {
+        if (state.isRunActive && !isServiceActive) {
             onServiceToggle(true)
         }
-
     }
-
     ObserveAsEvents(viewModel.events) { event ->
-        when(event) {
+        when (event) {
             is TrackerEvent.Error -> {
                 Toast.makeText(
                     context,
@@ -75,6 +74,7 @@ fun TrackerScreenRoot(
                     Toast.LENGTH_LONG
                 ).show()
             }
+
             TrackerEvent.RunFinished -> {
                 onServiceToggle(false)
             }
@@ -106,7 +106,7 @@ private fun TrackerScreen(
         ) == PackageManager.PERMISSION_GRANTED
         onAction(TrackerAction.OnBodySensorPermissionResult(hasBodySensorPermission))
 
-        val hasNotificationPermission = if(Build.VERSION.SDK_INT >= 33) {
+        val hasNotificationPermission = if (Build.VERSION.SDK_INT >= 33) {
             ContextCompat.checkSelfPermission(
                 context,
                 Manifest.permission.POST_NOTIFICATIONS
@@ -116,21 +116,31 @@ private fun TrackerScreen(
         }
 
         val permissions = mutableListOf<String>()
-        if(!hasBodySensorPermission) {
+        if (!hasBodySensorPermission) {
             permissions.add(Manifest.permission.BODY_SENSORS)
         }
-        if(!hasNotificationPermission && Build.VERSION.SDK_INT >= 33) {
+        if (!hasNotificationPermission && Build.VERSION.SDK_INT >= 33) {
             permissions.add(Manifest.permission.POST_NOTIFICATIONS)
         }
 
         permissionLauncher.launch(permissions.toTypedArray())
     }
 
+    AmbientObserver(
+        onEnterAmbient = {
+            onAction(TrackerAction.OnEnterAmbientMode(it.burnInProtectionRequired))
+        },
+        onExitAmbient = {
+            onAction(TrackerAction.OnExitAmbientMode)
+        }
+    )
+
     if (state.isConnectedPhoneNearby) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background),
+                .background(MaterialTheme.colorScheme.background)
+                .ambientMode(state.isAmbientMode, state.burnInProtectionRequired),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
@@ -203,6 +213,7 @@ private fun TrackerScreen(
                     Text(
                         text = stringResource(id = R.string.open_active_run_screen),
                         textAlign = TextAlign.Center,
+                        fontSize = 14.sp,
                         modifier = Modifier
                             .fillMaxWidth()
                             .padding(horizontal = 14.dp)
